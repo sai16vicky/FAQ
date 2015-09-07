fb791295e7185df77b191d8c5d83c4a6908212cf
sai16vicky <saiviknesh@dhcp037-054-018.wireless.buffalo.edu>
2015-09-07 12:46:03 -0400

    Data Simulation with Sparsity Condition

--- data_simulation_for_sparse_pgms.py
+++ data_simulation_for_sparse_pgms.py
@@ -6,18 +6,27 @@ import scipy
 import sets
 
 gen_prob_index = 0
-factor_model = [dict()]*100
+zero_index = 0
 
 # This function recursively fills a factor for each combination of the domain of its variables.
 def recursively_fill_values_of_domain(cur_factor, factor_index, factor, domain_dictionary, generated_probabilities, domain_list):
 		if factor_index == len(factor):
 				global gen_prob_index
 				global factor_model
-				factor_model[cur_factor][frozenset(domain_list)] = generated_probabilities[0][gen_prob_index]
-				gen_prob_index = gen_prob_index + 1
+				global zero_index
+				global number_of_zeros
+				toss = numpy.random.randint(0, 1)
+				tmp_domain_list = list(domain_list)
+				if toss == 1 or zero_index == number_of_zeros: 
+						tmp_domain_list.append(generated_probabilities[0][gen_prob_index])
+						gen_prob_index = gen_prob_index + 1
+						print tmp_domain_list
+				else :
+						tmp_domain_list.append(0)
+						zero_index = zero_index + 1
 				return 
-		tmp_domain_list = domain_list
 		for cur_var_domain in range(0, domain_dictionary[factor[factor_index]]):
+				tmp_domain_list = list(domain_list)
 				tmp_domain_list.append(cur_var_domain)
 				recursively_fill_values_of_domain(cur_factor, factor_index + 1, factor, domain_dictionary, generated_probabilities, tmp_domain_list)
 
@@ -25,6 +34,7 @@ def recursively_fill_values_of_domain(cur_factor, factor_index, factor, domain_d
 max_variables = input()
 no_of_variables = numpy.random.randint(2, max_variables)
 no_of_factors = numpy.random.randint(1, 2**max_variables - 1)
+factor_model = [list()]*no_of_factors
 factor_set = set()
 for each_factor in range(0, no_of_factors):
 		cur_factor = list()
@@ -48,19 +58,23 @@ for each_variable in range(1, no_of_variables + 1):
 print(domain_dictionary)
 
 # We need to get this feature up. As of now we are generating random models.
-# enter_sparsity = input()
 cur_factor = 0
 for factor in factor_set:
 		factor_size = 1
 		factor = sorted(list(factor))
+		print factor
 		for factor_var in factor:
 				factor_size = factor_size * domain_dictionary[factor_var]
+		sparsity_factor = input()
+		number_of_zeros = (sparsity_factor/100.0) * factor_size
+		non_zero_factor_size = factor_size - number_of_zeros
 		# The generated probabilities sum to 1 as we use Dirichlet distribution.
-		generated_probabilities = numpy.random.dirichlet(numpy.ones(factor_size), size=1).tolist()
+		generated_probabilities = numpy.random.dirichlet(numpy.ones(non_zero_factor_size), size=1).tolist()
 		gen_prob_index = 0
+		zero_index = 0
 		recursively_fill_values_of_domain(cur_factor, 0, factor, domain_dictionary, generated_probabilities, list())
-		print factor_model[cur_factor]
-		cur_factor += 1
+		print
+		cur_factor = cur_factor + 1
 
 		
 
